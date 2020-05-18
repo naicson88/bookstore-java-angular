73dbad02b3525588fbf4a6cfa0e022f346a0ea2f
naicson88 <naicson88@hotmail.com>
2020-05-18 00:17:09 -0300

    parte 40

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/common/book';
 import { BookService } from 'src/app/services/book.service';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,14 +13,29 @@ import { BookService } from 'src/app/services/book.service';
 export class BookListComponent implements OnInit {
 
   books : Book[];
-  
-  constructor(private _bookService: BookService) { }
+  currentCategoryId: number;
+
+  constructor(private _bookService: BookService,
+    private _activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listBooks();
+    
+    this._activeRoute.paramMap.subscribe(()=>{
+      this.listBooks();
+    })
+
   }
 
   listBooks(){
+
+    const hasCategoryId: boolean = this._activeRoute.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      this.currentCategoryId = +this._activeRoute.snapshot.paramMap.get('id');
+    } else {
+      this.currentCategoryId = 1;
+    }
+
     this._bookService.getBooks().subscribe(
       data => {
         this.books = data
